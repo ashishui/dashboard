c8a498bb8b11ff053d89aa1346d6e6a75d2f9a84
ashishui <ashishmishra221984@gmail.com>
2020-10-19 16:42:19 +0530

    dashboard created

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,18 @@
+import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+import { DefaultComponent } from './layouts/default/default.component';
+
+const routes: Routes = [{
+  path: '',
+  component: DefaultComponent,
+  children: [
+    { path: '', loadChildren: () => import('./modules/default/default.module').then(m => m.DefaultModule) },
+    { path: 'lazy', loadChildren: () => import('./modules/lazy/lazy.module').then(m => m.LazyModule) }
+  ]
+}];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
